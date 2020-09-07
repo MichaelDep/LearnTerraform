805872fa63e52af563a205a6a0733f958da88d5f
Michael Deputy <Michael.Deputy85@gmail.com>
2020-09-07 15:25:38 -0400

    .gitignore

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
-#
+# az
+# az login
+# az account set --subscription "Dep's MSDN"
+# az account show
+
+
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.20.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "dep" {
+  name     = "DepsEphemeralResources"
+  location = "East US"
+}
