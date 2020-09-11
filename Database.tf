f6eec1dad3a0d7e8e40e9490c459ec3c3e9fec60
Michael Deputy <Michael.Deputy85@gmail.com>
2020-09-10 20:56:35 -0400

    added subscription to provider

--- Database.tf
+++ Database.tf
@@ -2,6 +2,7 @@ provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
   version = "=2.20.0"
   alias  = "database"
+  subscription_id = "99db4c5e-6b9f-49d6-adaa-2d79d3e31ac5"
   features {}
 }
 
